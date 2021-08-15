45593c523ac3fcba8ee8f544390ef48b4e7a5909
justusmiddendorf <justus@noxt.de>
2021-08-15 20:14:31 +0200

    Basis

---
+++ app/src/gplay/java/com/nextcloud/talk/services/firebase/MagicFirebaseMessagingService.kt
@@ -0,0 +1,356 @@
+/*
+ * Nextcloud Talk application
+ *
+ * @author Mario Danic
+ * Copyright (C) 2017-2019 Mario Danic <mario@lovelyhq.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.nextcloud.talk.services.firebase
+
+import android.annotation.SuppressLint
+import android.app.Notification
+import android.app.PendingIntent
+import android.content.Intent
+import android.media.AudioAttributes
+import android.net.Uri
+import android.os.Bundle
+import android.os.Handler
+import android.text.TextUtils
+import android.util.Base64
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.emoji.text.EmojiCompat
+import androidx.work.Data
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import autodagger.AutoInjector
+import com.bluelinelabs.logansquare.LoganSquare
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.nextcloud.talk.R
+import com.nextcloud.talk.activities.MagicCallActivity
+import com.nextcloud.talk.api.NcApi
+import com.nextcloud.talk.application.NextcloudTalkApplication
+import com.nextcloud.talk.application.NextcloudTalkApplication.Companion.sharedApplication
+import com.nextcloud.talk.events.CallNotificationClick
+import com.nextcloud.talk.jobs.NotificationWorker
+import com.nextcloud.talk.jobs.PushRegistrationWorker
+import com.nextcloud.talk.models.RingtoneSettings
+import com.nextcloud.talk.models.SignatureVerification
+import com.nextcloud.talk.models.json.participants.Participant
+import com.nextcloud.talk.models.json.participants.ParticipantsOverall
+import com.nextcloud.talk.models.json.push.DecryptedPushMessage
+import com.nextcloud.talk.utils.ApiUtils
+import com.nextcloud.talk.utils.NotificationUtils
+import com.nextcloud.talk.utils.NotificationUtils.cancelAllNotificationsForAccount
+import com.nextcloud.talk.utils.NotificationUtils.cancelExistingNotificationWithId
+import com.nextcloud.talk.utils.NotificationUtils.createNotificationChannel
+import com.nextcloud.talk.utils.PushUtils
+import com.nextcloud.talk.utils.bundle.BundleKeys
+import com.nextcloud.talk.utils.bundle.BundleKeys.KEY_FROM_NOTIFICATION_START_CALL
+import com.nextcloud.talk.utils.bundle.BundleKeys.KEY_USER_ENTITY
+import com.nextcloud.talk.utils.preferences.AppPreferences
+import io.reactivex.Observer
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import okhttp3.JavaNetCookieJar
+import okhttp3.OkHttpClient
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
+import retrofit2.Retrofit
+import java.io.IOException
+import java.net.CookieManager
+import java.security.InvalidKeyException
+import java.security.NoSuchAlgorithmException
+import java.security.PrivateKey
+import javax.crypto.Cipher
+import javax.crypto.NoSuchPaddingException
+import javax.inject.Inject
+
+@AutoInjector(NextcloudTalkApplication::class)
+class MagicFirebaseMessagingService : FirebaseMessagingService() {
+    @JvmField
+    @Inject
+    var appPreferences: AppPreferences? = null
+
+    var isServiceInForeground: Boolean = false
+    private var decryptedPushMessage: DecryptedPushMessage? = null
+    private var signatureVerification: SignatureVerification? = null
+    private var handler: Handler = Handler()
+
+    @JvmField
+    @Inject
+    var retrofit: Retrofit? = null
+
+    @JvmField
+    @Inject
+    var okHttpClient: OkHttpClient? = null
+
+    @JvmField
+    @Inject
+    var eventBus: EventBus? = null
+
+    override fun onCreate() {
+        super.onCreate()
+        sharedApplication!!.componentApplication.inject(this)
+        eventBus?.register(this)
+    }
+
+    @Subscribe(threadMode = ThreadMode.BACKGROUND)
+    fun onMessageEvent(event: CallNotificationClick) {
+        isServiceInForeground = false
+        stopForeground(true)
+    }
+
+    override fun onDestroy() {
+        isServiceInForeground = false
+        eventBus?.unregister(this)
+        stopForeground(true)
+        handler.removeCallbacksAndMessages(null)
+        super.onDestroy()
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        sharedApplication!!.componentApplication.inject(this)
+        appPreferences!!.pushToken = token
+        val pushRegistrationWork = OneTimeWorkRequest.Builder(PushRegistrationWorker::class.java).build()
+        WorkManager.getInstance().enqueue(pushRegistrationWork)
+    }
+
+    @SuppressLint("LongLogTag")
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        sharedApplication!!.componentApplication.inject(this)
+        if (!remoteMessage.data["subject"].isNullOrEmpty() && !remoteMessage.data["signature"].isNullOrEmpty()) {
+            decryptMessage(remoteMessage.data["subject"]!!, remoteMessage.data["signature"]!!)
+        }
+    }
+
+    private fun decryptMessage(subject: String, signature: String) {
+        try {
+            val base64DecodedSubject = Base64.decode(subject, Base64.DEFAULT)
+            val base64DecodedSignature = Base64.decode(signature, Base64.DEFAULT)
+            val pushUtils = PushUtils()
+            val privateKey = pushUtils.readKeyFromFile(false) as PrivateKey
+            try {
+                signatureVerification = pushUtils.verifySignature(
+                    base64DecodedSignature,
+                    base64DecodedSubject
+                )
+                if (signatureVerification!!.signatureValid) {
+                    val cipher = Cipher.getInstance("RSA/None/PKCS1Padding")
+                    cipher.init(Cipher.DECRYPT_MODE, privateKey)
+                    val decryptedSubject = cipher.doFinal(base64DecodedSubject)
+                    decryptedPushMessage = LoganSquare.parse(
+                        String(decryptedSubject),
+                        DecryptedPushMessage::class.java
+                    )
+                    decryptedPushMessage?.apply {
+                        timestamp = System.currentTimeMillis()
+                        if (delete) {
+                            cancelExistingNotificationWithId(
+                                applicationContext,
+                                signatureVerification!!.userEntity,
+                                notificationId
+                            )
+                        } else if (deleteAll) {
+                            cancelAllNotificationsForAccount(applicationContext, signatureVerification!!.userEntity)
+                        } else if (deleteMultiple) {
+                            notificationIds.forEach {
+                                cancelExistingNotificationWithId(
+                                    applicationContext,
+                                    signatureVerification!!.userEntity,
+                                    it
+                                )
+                            }
+                        } else if (type == "call") {
+                            val fullScreenIntent = Intent(applicationContext, MagicCallActivity::class.java)
+                            val bundle = Bundle()
+                            bundle.putString(BundleKeys.KEY_ROOM_ID, decryptedPushMessage!!.id)
+                            bundle.putParcelable(KEY_USER_ENTITY, signatureVerification!!.userEntity)
+                            bundle.putBoolean(KEY_FROM_NOTIFICATION_START_CALL, true)
+                            fullScreenIntent.putExtras(bundle)
+
+                            fullScreenIntent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+                            val fullScreenPendingIntent = PendingIntent.getActivity(
+                                this@MagicFirebaseMessagingService,
+                                0,
+                                fullScreenIntent,
+                                PendingIntent.FLAG_UPDATE_CURRENT
+                            )
+
+                            val audioAttributesBuilder =
+                                AudioAttributes.Builder().setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                            audioAttributesBuilder.setUsage(AudioAttributes.USAGE_NOTIFICATION_COMMUNICATION_REQUEST)
+
+                            val ringtonePreferencesString: String? = appPreferences!!.callRingtoneUri
+                            val soundUri = if (TextUtils.isEmpty(ringtonePreferencesString)) {
+                                Uri.parse(
+                                    "android.resource://" + applicationContext.packageName +
+                                        "/raw/librem_by_feandesign_call"
+                                )
+                            } else {
+                                try {
+                                    val ringtoneSettings =
+                                        LoganSquare.parse(ringtonePreferencesString, RingtoneSettings::class.java)
+                                    ringtoneSettings.ringtoneUri
+                                } catch (exception: IOException) {
+                                    Uri.parse(
+                                        "android.resource://" +
+                                            applicationContext.packageName +
+                                            "/raw/librem_by_feandesign_call"
+                                    )
+                                }
+                            }
+
+                            val notificationChannelId = NotificationUtils.getNotificationChannelId(
+                                applicationContext.resources
+                                    .getString(R.string.nc_notification_channel_calls),
+                                applicationContext.resources
+                                    .getString(R.string.nc_notification_channel_calls_description),
+                                true,
+                                NotificationManagerCompat.IMPORTANCE_HIGH,
+                                soundUri!!,
+                                audioAttributesBuilder.build(),
+                                null,
+                                false
+                            )
+
+                            createNotificationChannel(
+                                applicationContext!!,
+                                notificationChannelId,
+                                applicationContext.resources
+                                    .getString(R.string.nc_notification_channel_calls),
+                                applicationContext.resources
+                                    .getString(R.string.nc_notification_channel_calls_description),
+                                true,
+                                NotificationManagerCompat.IMPORTANCE_HIGH,
+                                soundUri,
+                                audioAttributesBuilder.build(),
+                                null,
+                                false
+                            )
+
+                            val uri = Uri.parse(signatureVerification!!.userEntity.baseUrl)
+                            val baseUrl = uri.host
+
+                            val notification =
+                                NotificationCompat.Builder(this@MagicFirebaseMessagingService, notificationChannelId)
+                                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                                    .setCategory(NotificationCompat.CATEGORY_CALL)
+                                    .setSmallIcon(R.drawable.ic_call_black_24dp)
+                                    .setSubText(baseUrl)
+                                    .setShowWhen(true)
+                                    .setWhen(decryptedPushMessage!!.timestamp)
+                                    .setContentTitle(EmojiCompat.get().process(decryptedPushMessage!!.subject))
+                                    .setAutoCancel(true)
+                                    .setOngoing(true)
+                                    // .setTimeoutAfter(45000L)
+                                    .setContentIntent(fullScreenPendingIntent)
+                                    .setFullScreenIntent(fullScreenPendingIntent, true)
+                                    .setSound(soundUri)
+                                    .build()
+                            notification.flags = notification.flags or Notification.FLAG_INSISTENT
+                            isServiceInForeground = true
+                            checkIfCallIsActive(signatureVerification!!, decryptedPushMessage!!)
+                            startForeground(decryptedPushMessage!!.timestamp.toInt(), notification)
+                        } else {
+                            val messageData = Data.Builder()
+                                .putString(BundleKeys.KEY_NOTIFICATION_SUBJECT, subject)
+                                .putString(BundleKeys.KEY_NOTIFICATION_SIGNATURE, signature)
+                                .build()
+                            val pushNotificationWork =
+                                OneTimeWorkRequest.Builder(NotificationWorker::class.java).setInputData(messageData)
+                                    .build()
+                            WorkManager.getInstance().enqueue(pushNotificationWork)
+                        }
+                    }
+                }
+            } catch (e1: NoSuchAlgorithmException) {
+                Log.d(NotificationWorker.TAG, "No proper algorithm to decrypt the message " + e1.localizedMessage)
+            } catch (e1: NoSuchPaddingException) {
+                Log.d(NotificationWorker.TAG, "No proper padding to decrypt the message " + e1.localizedMessage)
+            } catch (e1: InvalidKeyException) {
+                Log.d(NotificationWorker.TAG, "Invalid private key " + e1.localizedMessage)
+            }
+        } catch (exception: Exception) {
+            Log.d(NotificationWorker.TAG, "Something went very wrong " + exception.localizedMessage)
+        }
+    }
+
+    private fun checkIfCallIsActive(
+        signatureVerification: SignatureVerification,
+        decryptedPushMessage: DecryptedPushMessage
+    ) {
+        val ncApi = retrofit!!.newBuilder()
+            .client(okHttpClient!!.newBuilder().cookieJar(JavaNetCookieJar(CookieManager())).build()).build()
+            .create(NcApi::class.java)
+        var hasParticipantsInCall = false
+        var inCallOnDifferentDevice = false
+
+        val apiVersion = ApiUtils.getConversationApiVersion(
+            signatureVerification.userEntity,
+            intArrayOf(ApiUtils.APIv4, 1)
+        )
+
+        ncApi.getPeersForCall(
+            ApiUtils.getCredentials(signatureVerification.userEntity.username, signatureVerification.userEntity.token),
+            ApiUtils.getUrlForCall(
+                apiVersion,
+                signatureVerification.userEntity.baseUrl,
+                decryptedPushMessage.id
+            )
+        )
+            .takeWhile {
+                isServiceInForeground
+            }
+            .subscribeOn(Schedulers.io())
+            .subscribe(object : Observer<ParticipantsOverall> {
+                override fun onSubscribe(d: Disposable) {
+                }
+
+                override fun onNext(participantsOverall: ParticipantsOverall) {
+                    val participantList: List<Participant> = participantsOverall.ocs.data
+                    hasParticipantsInCall = participantList.isNotEmpty()
+                    if (hasParticipantsInCall) {
+                        for (participant in participantList) {
+                            if (participant.userId == signatureVerification.userEntity.userId) {
+                                inCallOnDifferentDevice = true
+                                break
+                            }
+                        }
+                    }
+
+                    if (!hasParticipantsInCall || inCallOnDifferentDevice) {
+                        stopForeground(true)
+                        handler.removeCallbacksAndMessages(null)
+                    } else if (isServiceInForeground) {
+                        handler.postDelayed(
+                            {
+                                checkIfCallIsActive(signatureVerification, decryptedPushMessage)
+                            },
+                            5000
+                        )
+                    }
+                }
+
+                override fun onError(e: Throwable) {}
+                override fun onComplete() {
+                }
+            })
+    }
+}
---
+++ scripts/analysis/analysis-wrapper.sh
@@ -0,0 +1,124 @@
+#!/usr/bin/env bash
+
+#1: GIT_USERNAME
+#2: GIT_TOKEN
+#3: BRANCH
+#4: LOG_USERNAME
+#5: LOG_PASSWORD
+#6: DRONE_BUILD_NUMBER
+#7: PULL_REQUEST_NUMBER
+
+stableBranch="master"
+repository="talk"
+
+ruby scripts/analysis/lint-up.rb $1 $2 $3
+lintValue=$?
+
+ruby scripts/analysis/findbugs-up.rb $1 $2 $3
+findbugsValue=$?
+
+# exit codes:
+# 0: count was reduced
+# 1: count was increased
+# 2: count stayed the same
+
+echo "Branch: $3"
+
+if [ $3 = $stableBranch ]; then
+    echo "New findbugs result for $stableBranch at: https://www.kaminsky.me/nc-dev/$repository-findbugs/$stableBranch.html"
+    curl -u $4:$5 -X PUT https://nextcloud.kaminsky.me/remote.php/webdav/$repository-findbugs/$stableBranch.html --upload-file app/build/reports/spotbugs/spotbugs.html
+
+    summary=$(sed -n "/<h1>Summary<\/h1>/,/<h1>Warnings<\/h1>/p" app/build/reports/spotbugs/spotbugs.html | head -n-1 | sed s'/<\/a>//'g | sed s'/<a.*>//'g | sed s"/Summary/SpotBugs ($stableBranch)/" | tr "\"" "\'" | tr -d "\r\n")
+    curl -u $4:$5 -X PUT -d "$summary" https://nextcloud.kaminsky.me/remote.php/webdav/$repository-findbugs/findbugs-summary-$stableBranch.html
+
+    if [ $lintValue -ne 1 ]; then
+        echo "New lint result for $stableBranch at: https://www.kaminsky.me/nc-dev/$repository-lint/$stableBranch.html"
+        curl -u $4:$5 -X PUT https://nextcloud.kaminsky.me/remote.php/webdav/$repository-lint/$stableBranch.html --upload-file app/build/reports/lint/lint.html
+        exit 0
+    fi
+else
+    if [ -e $6 ]; then
+        6=$stableBranch"-"$(date +%F)
+    fi
+    echo "New lint results at https://www.kaminsky.me/nc-dev/$repository-lint/$6.html"
+    curl 2>/dev/null -u $4:$5 -X PUT https://nextcloud.kaminsky.me/remote.php/webdav/$repository-lint/$6.html --upload-file app/build/reports/lint/lint.html
+
+    echo "New findbugs results at https://www.kaminsky.me/nc-dev/$repository-findbugs/$6.html"
+    curl 2>/dev/null -u $4:$5 -X PUT https://nextcloud.kaminsky.me/remote.php/webdav/$repository-findbugs/$6.html --upload-file app/build/reports/spotbugs/spotbugs.html
+
+    # delete all old comments
+    oldComments=$(curl 2>/dev/null -u $1:$2 -X GET https://api.github.com/repos/nextcloud/talk-android/issues/$7/comments | jq '.[] | (.id |tostring) + "|" + (.user.login | test("nextcloud-android-bot") | tostring) ' | grep true | tr -d "\"" | cut -f1 -d"|")
+
+    echo $oldComments | while read comment ; do
+        curl 2>/dev/null -u $1:$2 -X DELETE https://api.github.com/repos/nextcloud/talk-android/issues/comments/$comment
+    done
+
+    # lint and findbugs file must exist
+    if [ ! -s app/build/reports/lint/lint.html ] ; then
+        echo "lint.html file is missing!"
+        exit 1
+    fi
+
+    if [ ! -s app/build/reports/spotbugs/spotbugs.html ] ; then
+        echo "spotbugs.html file is missing!"
+        exit 1
+    fi
+
+    # add comment with results
+    lintResultNew=$(grep "Lint Report.* [0-9]* warning" app/build/reports/lint/lint.html | cut -f2 -d':' |cut -f1 -d'<')
+
+    lintErrorNew=$(echo $lintResultNew | grep "[0-9]* error" -o | cut -f1 -d" ")
+    if ( [ -z $lintErrorNew ] ); then
+        lintErrorNew=0
+    fi
+
+    lintWarningNew=$(echo $lintResultNew | grep "[0-9]* warning" -o | cut -f1 -d" ")
+    if ( [ -z $lintWarningNew ] ); then
+        lintWarningNew=0
+    fi
+
+    lintResultOld=$(curl 2>/dev/null https://raw.githubusercontent.com/nextcloud/talk-android/$stableBranch/scripts/analysis/lint-results.txt)
+    lintErrorOld=$(echo $lintResultOld | grep "[0-9]* error" -o | cut -f1 -d" ")
+    if ( [ -z $lintErrorOld ] ); then
+        lintErrorOld=0
+    fi
+
+    lintWarningOld=$(echo $lintResultOld | grep "[0-9]* warning" -o | cut -f1 -d" ")
+    if ( [ -z $lintWarningOld ] ); then
+        lintWarningOld=0
+    fi
+
+    lintResult="<h1>Lint</h1><table width='500' cellpadding='5' cellspacing='2'><tr class='tablerow0'><td>Type</td><td><a href='https://www.kaminsky.me/nc-dev/"$repository"-lint/"$stableBranch".html'>$stableBranch</a></td><td><a href='https://www.kaminsky.me/nc-dev/"$repository"-lint/"$6".html'>PR</a></td></tr><tr class='tablerow1'><td>Warnings</td><td>"$lintWarningOld"</td><td>"$lintWarningNew"</td></tr><tr class='tablerow0'><td>Errors</td><td>"$lintErrorOld"</td><td>"$lintErrorNew"</td></tr></table>"
+    findbugsResultNew=$(sed -n "/<h1>Summary<\/h1>/,/<h1>Warnings<\/h1>/p" app/build/reports/spotbugs/spotbugs.html |head -n-1 | sed s'/<\/a>//'g | sed s'/<a.*>//'g | sed s"#Summary#<a href=\"https://www.kaminsky.me/nc-dev/$repository-findbugs/$6.html\">SpotBugs</a> (new)#" | tr "\"" "\'" | tr -d "\n")
+    findbugsResultOld=$(curl 2>/dev/null https://www.kaminsky.me/nc-dev/$repository-findbugs/findbugs-summary-$stableBranch.html | tr "\"" "\'" | tr -d "\r\n" | sed s"#SpotBugs#<a href=\"https://www.kaminsky.me/nc-dev/$repository-findbugs/$stableBranch.html\">SpotBugs</a>#" | tr "\"" "\'" | tr -d "\n")
+
+
+    if ( [ $lintValue -eq 1 ] ) ; then
+        lintMessage="<h1>Lint increased!</h1>"
+    fi
+
+    if ( [ $findbugsValue -eq 1 ] ) ; then
+        findbugsMessage="<h1>SpotBugs increased!</h1>"
+    fi
+
+    # check for NotNull
+    if [[ $(grep org.jetbrains.annotations src/main/* -ir -c) -gt 0 ]] ; then
+        notNull="org.jetbrains.annotations.NotNull is used. Please use androidx.annotation.NonNull instead.<br><br>"
+    fi
+
+    curl -u $1:$2 -X POST https://api.github.com/repos/nextcloud/talk-android/issues/$7/comments -d "{ \"body\" : \"$lintResult $findbugsResultNew $findbugsResultOld $lintMessage $findbugsMessage $notNull\" }"
+
+    if [ ! $lintValue -eq 2 ]; then
+        exit $lintValue
+    fi
+
+    if [[ -n $notNull ]]; then
+        exit 1
+    fi
+
+    if [ $findbugsValue -eq 2 ]; then
+        exit 0
+    else
+        exit $findbugsValue
+    fi
+fi
